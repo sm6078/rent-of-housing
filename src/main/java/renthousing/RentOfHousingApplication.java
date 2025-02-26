3db5b427e1ba9c7f982c663bcc121f431d7ab853
Sergei Rodin <sm6078@mail.ru>
2025-02-26 18:23:17 +0300

    create structure for project

---
+++ src/main/java/renthousing/RentOfHousingApplication.java
@@ -0,0 +1,11 @@
+package renthousing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentOfHousingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RentOfHousingApplication.class, args);
+    }
+}
---
+++ src/test/java/renthousing/RentOfHousingApplicationTests.java
@@ -0,0 +1,12 @@
+package renthousing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentOfHousingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
